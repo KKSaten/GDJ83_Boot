ab756855c21f49b99da5190ac5f776e251257f65
LeeSiwoo <cocoar147@naver.com>
2024-09-04 15:45:03 +0900

    .

--- src/main/java/com/lsw/app/configs/Fileconfig.java
+++ src/main/java/com/lsw/app/configs/Fileconfig.java
@@ -2,13 +2,17 @@ package com.lsw.app.configs;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
 //설정 class, XML
-@Configuration
 
 //WebMVCConfigure 구현
+@Configuration
+@Slf4j
 public class Fileconfig implements WebMvcConfigurer {
 	
 	@Value("${app.url.path}")
